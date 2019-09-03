e7b12c49cdb0a40102c78bb6d1e3fd735c9ba63b
knsh14 <knsh14@mercari.com>
2019-09-03 18:02:10 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/knsh14/astviewer
+
+go 1.12
+
+require (
+	gioui.org/ui v0.0.0-20190902131856-a06d8a39f534
+	github.com/knsh14/astree v0.0.0-20190816154512-02dadd236648
+	golang.org/x/image v0.0.0-20190902063713-cb417be4ba39
+)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"go/parser"
+	"go/token"
+	"image"
+	"image/color"
+	"log"
+
+	"gioui.org/ui"
+	"gioui.org/ui/app"
+	"gioui.org/ui/f32"
+	"gioui.org/ui/key"
+	"gioui.org/ui/layout"
+	"gioui.org/ui/measure"
+	"gioui.org/ui/paint"
+	"gioui.org/ui/text"
+	"github.com/knsh14/astree"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/font/sfnt"
+)
+
+const initial = `
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+}`
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		if err := loop(w); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	app.Main()
+}
+
+func loop(w *app.Window) error {
+	var cfg app.Config
+	regular, _ := sfnt.Parse(goregular.TTF)
+	var faces measure.Faces
+	ops := new(ui.Ops)
+	edtr := &text.Editor{
+		Face:       faces.For(regular, ui.Dp(20)),
+		Submit:     false,
+		SingleLine: false,
+		Hint:       initial,
+	}
+	input := ""
+	edtr.SetText(initial)
+	l := &layout.List{Axis: layout.Vertical}
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case app.UpdateEvent:
+			cfg = e.Config
+			faces.Reset(&cfg)
+			ops.Reset()
+			flex := layout.Flex{Spacing: layout.SpaceBetween}
+			cs := layout.RigidConstraints(e.Size)
+			flex.Init(ops, cs)
+			queue := w.Queue()
+			for e, ok := edtr.Next(&cfg, queue); ok; e, ok = edtr.Next(&cfg, queue) {
+				if _, ok = e.(text.ChangeEvent); ok {
+					fs := token.NewFileSet()
+					code := edtr.Text()
+					f, err := parser.ParseFile(fs, "main.go", code, 0)
+					if err != nil {
+						input = err.Error()
+					} else {
+						var buf bytes.Buffer
+						astree.File(&buf, fs, f)
+						input = buf.String()
+					}
+				}
+			}
+			cs = flex.Flexible(0.49)
+			dims := edtr.Layout(&cfg, queue, ops, cs)
+			leftside := flex.End(dims)
+
+			// draw a black line to separate
+			cs = flex.Flexible(0.01)
+			square := f32.Rectangle{
+				Max: f32.Point{
+					X: float32(cs.Width.Max),
+					Y: float32(cs.Height.Max),
+				},
+			}
+			paint.ColorOp{Color: color.RGBA{A: 0xff}}.Add(ops)
+			paint.PaintOp{Rect: square}.Add(ops)
+			dims = layout.Dimensions{Size: image.Point{X: cs.Width.Max, Y: cs.Height.Max}}
+			centerline := flex.End(dims)
+
+			cs = flex.Flexible(0.5)
+			if l.Dragging() {
+				key.HideInputOp{}.Add(ops)
+			}
+			for l.Init(&cfg, queue, ops, cs, 1); l.More(); l.Next() {
+				dims = text.Label{
+					Face: faces.For(regular, ui.Dp(20)),
+					Text: input,
+				}.Layout(ops, l.Constraints())
+				l.End(dims)
+			}
+			rightside := flex.End(l.Layout())
+
+			dims = flex.Layout(leftside, centerline, rightside)
+			w.Update(ops)
+		}
+	}
+}
